71c582b864fa2716cfbef2d28e8f34313c9563b2
Alex <alexjsgood@gmail.com>
2014-08-28 18:51:52 +0100

    Various bits and pieces for PyPi

--- setup.py
+++ setup.py
@@ -9,4 +9,7 @@ setup(
     version='0.0.1',
     modules=['url_matchers'],
     install_requires=reqs,
+    author="Alex Good",
+    author_email="alex@makerlabs.co.uk",
+    url="https://github.com/alexjg/url-matchers",
 )
