0a9601c4ee085c38f660e6d40c98256098576a92
Alex <alexjsgood@gmail.com>
2014-08-28 18:55:26 +0100

    Use absolute path for requirements file

--- setup.py
+++ setup.py
@@ -1,7 +1,9 @@
 from distutils.core import setup
+from os import path
 from pip.req import parse_requirements
 
-install_reqs = parse_requirements("./requirements.txt")
+requirements_location = path.join(path.dirname(__file__), "requirements.txt")
+install_reqs = parse_requirements(requirements_location)
 reqs = [str(ir.req) for ir in install_reqs]
 
 setup(
