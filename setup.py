69933ca62d53e700c1d542ab0462d11351c6703e
Alex Good <alex@makerlabs.co.uk>
2016-06-07 14:24:39 +0100

    Don't use pip.parse_requirements

--- setup.py
+++ setup.py
@@ -2,16 +2,16 @@ from distutils.core import setup
 from os import path
 from pip.req import parse_requirements
 
-requirements_location = path.join(path.dirname(__file__), "requirements.txt")
-install_reqs = parse_requirements(requirements_location)
-reqs = [str(ir.req) for ir in install_reqs]
 
 setup(
     name='url-matchers',
     description="Url matchers for PyHamcrest",
-    version='0.0.1',
+    version='0.0.2',
     modules=['url_matchers'],
-    install_requires=reqs,
+    install_requires = [
+        "PyHamcrest>=1.8.0",
+        "six>=1.7.3",
+    ],
     author="Alex Good",
     author_email="alex@makerlabs.co.uk",
     url="https://github.com/alexjg/url-matchers",
