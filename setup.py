f95ef334af5279b8affa2acf165ba7132d70744b
Alex Good <alex@makerlabs.co.uk>
2016-06-07 17:26:56 +0100

    Pointless version bump

--- setup.py
+++ setup.py
@@ -1,13 +1,12 @@
-from distutils.core import setup
+from setuptools import setup
 from os import path
 from pip.req import parse_requirements
-
 
 setup(
     name='url-matchers',
     description="Url matchers for PyHamcrest",
-    version='0.0.2',
-    modules=['url_matchers'],
+    version='0.0.3',
+    py_modules=['url_matchers'],
     install_requires = [
         "PyHamcrest>=1.8.0",
         "six>=1.7.3",
